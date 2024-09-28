daf6bad9d023893dc92c403f345c21217827befc
lowskill <binocle101@gmail.com>
2024-09-28 19:50:14 +0300

    added -r flag

--- low_grep.py
+++ low_grep.py
@@ -1,102 +1,94 @@
 import argparse
 import re
-from operator import xor
 import os
 
+def custom_print(*args: object, end: object = '', **kwargs: object) -> None:
+    print(*args, end=end, **kwargs)
 
-def grep(args):
-	pass
+def parse_patterns(args: argparse.Namespace) -> list:
+    if args.regexp:
+        return args.regexp
+    elif args.pattern:
+        return [args.pattern]
+    else:
+        raise ValueError("No pattern provided")
 
+def get_flags(args: argparse.Namespace) -> int:
+    flags = re.IGNORECASE if args.i else 0
+    if args.extended_regexp:
+        flags |= re.VERBOSE
+    return flags
 
-def custom_print(*args, end='', **kwargs):
-	print(*args, end=end, **kwargs)
+def process_file(file: str, patterns: list, pattern_flags: int, args: argparse.Namespace, root: str = None):
+    matches = []
+    try:
+        with open(file, 'r', encoding='UTF-8') as f:
+            for line_number, line in enumerate(f, start=1):
+                line = line.strip()
+                for pattern in patterns:
+                    match_pattern = re.search(pattern, line, flags=pattern_flags) if not args.w else re.search(
+                        rf'\b{pattern}\b', line, flags=pattern_flags)
+                    if (bool(match_pattern) != args.v):
+                        matches.append((line_number, line))
+                        break
+    except FileNotFoundError:
+        custom_print(f"{file}: No such file or directory")
+    return matches
 
+def print_matches(matches: list, file: str, args: argparse.Namespace, root: str = None):
+    if args.c:
+        custom_print(f"{len(matches)}\n")
+    elif args.l:
+        if matches:
+            custom_print(f"{file}\n")
+    else:
+        for line_number, line in matches:
+            if (len(args.file) > 1 and not args.q):
+                if root:
+                    custom_print(f"{os.path.join(root, file)}:")
+                else:
+                    custom_print(f"{file}:")
+            if args.n:
+                custom_print(f"{line_number}:")
+            custom_print(f"{line}\n")
 
-def line_flag(args: argparse.Namespace):
-	count_of_file = len(args.file)
-	print(f"[LOG INFO] args: {args}")
-	print(f"[LOG INFO] count_of_file: {count_of_file}")
-	flags = re.IGNORECASE if args.i else 0
+def grep(args: argparse.Namespace):
+    patterns = parse_patterns(args)
+    pattern_flags = get_flags(args)
 
-	# Если передан флаг -e, используем его шаблоны, иначе используем позиционный шаблон
-	patterns = args.regexp if args.regexp else [args.pattern]
-
-	pattern_flags = flags | re.VERBOSE if args.extended_regexp else flags
-
-	count_of_match = 0
-	filenames_with_matches = []
-
-	for file in args.file:
-		if args.r and os.path.isdir(file):
-			for root, _, files in os.walk(file):
-				for f in files:
-					process_file(os.path.join(root, f), patterns, pattern_flags, args, count_of_match,
-								 filenames_with_matches)
-		else:
-			process_file(file, patterns, pattern_flags, args, count_of_match, filenames_with_matches)
-
-	if args.c and not args.l:
-		custom_print(count_of_match)
-	if args.l:
-		for filename in filenames_with_matches:
-			custom_print(filename)
-
-
-def process_file(file, patterns, pattern_flags, args, count_of_match, filenames_with_matches):
-	try:
-		with open(file, 'r', encoding='UTF-8') as f:
-			for line_number, line in enumerate(f, start=1):
-				line = line.strip()
-				match_found = False
-
-				for pattern in patterns:
-					match_pattern = re.search(pattern, line, flags=pattern_flags) if not args.w else re.search(
-						rf'\b{pattern}\b', line, flags=pattern_flags)
-					if xor(bool(match_pattern),
-						   args.v):  # (match_pattern and not args.v) or (not match_pattern and args.v)
-						match_found = True
-						count_of_match += 1
-						if file not in filenames_with_matches:
-							filenames_with_matches.append(file)
-						break
-
-				if match_found:
-					if len(args.file) > 1 and not args.q:
-						custom_print(f"{file}:")
-
-					if args.n and not args.l:
-						custom_print(f"{line_number}:")
-
-					custom_print(f"{line}\n")
-	except Exception as e:
-		print(f"low_grep: {file}: No such file or directory")
-
+    for file in args.file:
+        if args.r and os.path.isdir(file):
+            for root, _, files in os.walk(file):
+                for f in files:
+                    full_path = os.path.join(root, f)
+                    matches = process_file(full_path, patterns, pattern_flags, args, root)
+                    if matches: custom_print(f"{full_path}:")
+                    print_matches(matches, full_path, args, root)
+        else:
+            matches = process_file(file, patterns, pattern_flags, args)
+            print_matches(matches, file, args)
 
 def main():
-	parser = argparse.ArgumentParser(prog='MyGrep')
-	parser.add_argument('-n', help='custom_print line number with output lines', action='store_true')
-	parser.add_argument('-v', help='invert match (select non-matching lines)', action='store_true')
-	parser.add_argument('-i', help='ignore case distinctions in patterns and data', action='store_true')
-	parser.add_argument('-E', '--extended-regexp', action='store_true', help='Use extended regular expressions')
-	parser.add_argument('-e', '--regexp', action='append', help='Multiple patterns to search', type=str)
-	parser.add_argument('-w', action='store_true', help='Match whole words only')
-	parser.add_argument('-c', action='store_true', help='Count the number of occurrences of the provided pattern')
-	parser.add_argument('-l', action='store_true', help='Display file names with matches only')
-	parser.add_argument('-q', action='store_true',
-						help='don`t display file names with matches only analog -h in original grep')
-	parser.add_argument('-r', action='store_true', help='Recursively search directories')
-	group = parser.add_mutually_exclusive_group()
-	group.add_argument('pattern', help='pattern to search', type=str, nargs='?', action='store')
-	parser.add_argument('file', nargs='+', help='file to search', type=str)
+    parser = argparse.ArgumentParser(prog='MyGrep')
+    parser.add_argument('-n', help='print line number with output lines', action='store_true')
+    parser.add_argument('-v', help='invert match (select non-matching lines)', action='store_true')
+    parser.add_argument('-i', help='ignore case distinctions in patterns and data', action='store_true')
+    parser.add_argument('-E', '--extended-regexp', action='store_true', help='Use extended regular expressions')
+    parser.add_argument('-e', '--regexp', action='append', help='Multiple patterns to search', type=str)
+    parser.add_argument('-w', action='store_true', help='Match whole words only')
+    parser.add_argument('-c', action='store_true', help='Count the number of occurrences of the provided pattern')
+    parser.add_argument('-l', action='store_true', help='Display file names with matches only')
+    parser.add_argument('-q', action='store_true', help="don't display file names with matches only (analog -h in original grep)")
+    parser.add_argument('-r', action='store_true', help='Recursively search directories')
+    parser.add_argument('pattern', help='pattern to search', type=str, nargs='?')
+    parser.add_argument('file', nargs='+', help='file to search', type=str)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	# Если не передан флаг -e и нет позиционного шаблона, выводим ошибку
-	if not args.regexp and not args.pattern:
-		parser.error("You must provide at least one pattern using -e or as a positional argument.")
+    if not args.regexp and not args.pattern:
+        parser.error("You must provide at least one pattern using -e or as a positional argument.")
 
-	line_flag(args)
-
+    grep(args)
 
 if __name__ == '__main__':
-	main()
+    main()
